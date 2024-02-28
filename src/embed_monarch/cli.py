e11d619679cffbc6210df518a20d5e03d5eb3639
Justin Reese <justaddcoffee@gmail.com>
2024-02-28 12:47:20 -0500

    Initial commit

---
+++ src/embed_monarch/cli.py
@@ -0,0 +1,44 @@
+"""Command line interface for OLDembed_monarch."""
+import logging
+
+import click
+
+from embed_monarch import __version__
+from embed_monarch.main import demo
+
+__all__ = [
+    "main",
+]
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+@click.version_option(__version__)
+def main(verbose: int, quiet: bool):
+    """
+    CLI for OLDembed_monarch.
+
+    :param verbose: Verbosity while running.
+    :param quiet: Boolean to be quiet or verbose.
+    """
+    if verbose >= 2:
+        logger.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    else:
+        logger.setLevel(level=logging.WARNING)
+    if quiet:
+        logger.setLevel(level=logging.ERROR)
+
+
+@main.command()
+def run():
+    """Run the OLDembed_monarch's demo command."""
+    demo()
+
+
+if __name__ == "__main__":
+    main()
